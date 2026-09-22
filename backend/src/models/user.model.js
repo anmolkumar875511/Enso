14e1e7bfaa051c0a048a7e95ea8b544ce2b09efa
Anmol Kumar <anmolkumar875511@gmail.com>
2026-09-23 00:01:43 +0530

    user model added

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    authProvider: {
+        type: String,
+        enum: ['local', 'google'],
+        default: 'local'
+    },
+    googleId: {
+        type:String,
+        default: null
+    }
+}, {
+        timestamps: true
+});
+
+export default mongoose.model('User', userSchema);
